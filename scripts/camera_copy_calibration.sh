621334be92c605aa1f05933553f354ec7394b5d5
tviks <tviks879@gmail.com>
2022-04-27 23:19:27 +0300

    add new camera node

--- scripts/camera_copy_calibration.sh
+++ scripts/camera_copy_calibration.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm -rf ~/.ros/camera_info/
 mkdir ~/.ros/camera_info/
-cp include/camera.yaml ~/.ros/camera_info/
+cp include/left_camera.yaml ~/.ros/camera_info/
+cp include/right_camera.yaml ~/.ros/camera_info/
