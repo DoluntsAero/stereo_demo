c4ed91a27f07e470eeddfb7f62e776eb6599b9ff
tviks <tviks879@gmail.com>
2022-04-27 23:23:30 +0300

    update

---
+++ docs/uvc_camera.md
@@ -0,0 +1,25 @@
+# Libvuc
+
+## Ссылки
+
+[uvc_camera](http://wiki.ros.org/uvc_camera)
+
+## Установка 
+
+### Зависимотси 
+
+```
+sudo apt-get install libv4l-dev
+```
+
+### Сборка
+```
+cd ~/catkin_ws/src
+```
+```
+git clone https://github.com/ros-drivers/camera_umd
+```
+```
+catkin_make
+```
+
--- scripts/camera_calibration.sh
+++ scripts/camera_calibration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-roslaunch launch/libvuc_camera.launch &
+roslaunch launch/stereo_start.launch &
 
 rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.036 right:=/stereo/right/image_raw left:=/stereo/left/image_raw left_camera:=/stereo/left right_camera:=/stereo/right --approximate=0.01
