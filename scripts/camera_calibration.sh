a87f290e52ea88c21933994f99e5d564ff790c46
tviks <tviks879@gmail.com>
2022-04-27 20:29:42 +0300

    fix

--- scripts/camera_calibration.sh
+++ scripts/camera_calibration.sh
@@ -3,5 +3,3 @@
 roslaunch launch/libvuc_camera.launch &
 
 rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.036 right:=/stereo/right/image_raw left:=/stereo/left/image_raw left_camera:=/stereo/left right_camera:=/stereo/right --approximate=0.01
-
-rosrun stereo_image_proc stereo_image_proc /left/image_raw:=/camera/fisheye1/image_raw /left/camera_info:=/camera/fisheye1/camera_info /right/image_raw:=/camera/fisheye2/image_raw /right/camera_info:=/camera/fisheye2/camera_info
