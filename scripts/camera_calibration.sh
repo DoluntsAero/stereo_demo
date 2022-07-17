b24c5c748305a77602871a91ba450191e56fd306
Roman Lyskov <lyskov8@gmail.com>
2022-07-17 16:29:23 +0300

    FD-1 code update
    Stereo works with uvc_cam
    Avoidance works
    Arming granted by OAS

--- scripts/camera_calibration.sh
+++ scripts/camera_calibration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-roslaunch launch/stereo_start.launch &
+roslaunch launch/camera_start.launch &
 
-rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.036 right:=/stereo/right/image_raw left:=/stereo/left/image_raw left_camera:=/stereo/left right_camera:=/stereo/right --approximate=0.01
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.036 right:=/stereo/right/image_raw left:=/stereo/left/image_raw /left_camera/set_camera_info:=/stereo/left/set_camera_info /right_camera/set_camera_info:=/stereo/right/set_camera_info --approximate=0.01 
--- scripts/disparity_config.sh
+++ scripts/disparity_config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-roslaunch launch/stereo_start.launch &
+roslaunch launch/camera_start.launch &
 
-rosrun stereo_image_proc stereo_image_proc __ns:=stereo _approximate_sync:=true &
+rosrun stereo_image_proc stereo_image_proc __ns:=stereo _approximate_sync:=True &
 
-rosrun image_view stereo_view stereo:=/stereo image:=image_rect_color _approximate_sync:=true &
+rosrun image_view stereo_view stereo:=/stereo image:=image_rect_color _approximate_sync:=True &
 
 rosrun rqt_reconfigure rqt_reconfigure
